a8165f1526e037588c6d5f292d69058f442baa7d
Mattia_Dispoto <mdispoto@studenti.apuliadiitalmaker.it>
2024-05-05 22:37:25 +0200

    asdasd

---
+++ src/app/components/books/book-details/book-details.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import { Book } from '../../../models/book.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrl: './book-details.component.scss',
+})
+export class BookDetailsComponent implements OnInit {
+  model: Book = {
+    title: '',
+    author: '',
+    created: new Date(),
+    updated: new Date(),
+    category: '',
+  };
+
+  id: number | undefined = undefined;
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route?.params?.subscribe((params) => {
+      this.id = params['id'];
+      if (this.id) {
+        this.get(this.id);
+      }
+    });
+  }
+
+  get(id: number) {
+    this.apiService.GetBook(id).subscribe({
+      next: (response: Book) => {
+        this.model = response;
+      },
+      error: (error) => console.error(error),
+    });
+  }
+
+  save() {
+    if (!this.id)
+      this.apiService
+        .CreateBook(this.model)
+        .subscribe({ complete: () => this.router.navigateByUrl('/books') });
+    else this.apiService.UpdateBook(this.model).subscribe();
+  }
+}
---
+++ src/app/models/book.interface.ts
@@ -0,0 +1,8 @@
+export interface Book{
+    id?: number,
+    title: string,
+    author: string,
+    created?: Date,
+    updated?: Date,
+    category: string,
+}
